11e1b63108c39bc15f30be9b60d32a3d1c99d489
Subhan Gadirli <subhanqedirli0@gmail.com>
2026-09-13 19:47:43 +0400

    feat(router): map content paths to pretty URLs with conflicts
    - Add Plombir::Router.route/routes: pretty URLs, permalink
      overrides, slugify, duplicate detection with both sources
    - Add router specs: routing table, permalinks, slugs, conflicts
    
    Verified: crystal spec --error-on-warnings 28/0/0, tool format clean.

---
+++ spec/router/router_spec.cr
@@ -0,0 +1,39 @@
+require "../spec_helper"
+
+describe Plombir::Router do
+  it "maps the routing table" do
+    Plombir::Router.route("index.md").should eq(route("/", "index.html"))
+    Plombir::Router.route("about.md").should eq(route("/about/", "about/index.html"))
+    Plombir::Router.route("posts/hello.md").should eq(route("/posts/hello/", "posts/hello/index.html"))
+    Plombir::Router.route("posts/nested/index.md").should eq(route("/posts/nested/", "posts/nested/index.html"))
+  end
+
+  it "honors permalink overrides" do
+    Plombir::Router.route("posts/hello.md", "/custom/url").should eq(route("/custom/url/", "custom/url/index.html"))
+    Plombir::Router.route("posts/hello.md", "custom/").should eq(route("/custom/", "custom/index.html"))
+  end
+
+  it "slugifies unsafe segments" do
+    Plombir::Router.route("My Post!.md").url.should eq("/my-post/")
+    Plombir::Router.slugify("Hello, World!").should eq("hello-world")
+    Plombir::Router.slugify("!!!").should eq("page")
+  end
+
+  it "detects duplicate routes with both sources" do
+    ex = expect_raises(Plombir::Router::Conflict) do
+      Plombir::Router.routes([
+        {"about.md", nil},
+        {"about/index.md", nil},
+      ])
+    end
+
+    ex.url.should eq("/about/")
+    ex.message.to_s.should contain("about.md")
+    ex.message.to_s.should contain("about/index.md")
+    ex.message.to_s.should contain("permalink:")
+  end
+end
+
+private def route(url : String, output : String) : Plombir::Router::Route
+  Plombir::Router::Route.new(url, output)
+end
--- src/plombir.cr
+++ src/plombir.cr
@@ -4,6 +4,7 @@
 # Licensed under the Mozilla Public License 2.0 (MPL-2.0).
 require "yaml"
 require "./content/loader"
+require "./router/router"
 require "./frontmatter/parser"
 require "./markdown/renderer"
 require "./scaffold/templates"
