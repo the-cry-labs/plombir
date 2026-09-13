7883bd92b6f82b707ba4f38f3ed0731ee80c8642
Subhan Gadirli <subhanqedirli0@gmail.com>
2026-09-13 19:05:37 +0400

    Initial commit: Crystal library scaffold

---
+++ spec/plombir_spec.cr
@@ -0,0 +1,7 @@
+require "./spec_helper"
+
+describe Plombir do
+  it "has a version" do
+    Plombir::VERSION.should_not be_nil
+  end
+end
---
+++ src/plombir.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Plombir`
+module Plombir
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
